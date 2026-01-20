3b86b617e129f2056358e2b4ac45d1a62d180438
norah-chipoyera3 <norchipoyera@gmail.com>
2026-01-20 15:35:54 +0200

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -1,1 +1,69 @@
+--this code is for creating bronze tables
+--when creating tables first check the meta data from the source systems
 
+If object_id ('bronze.crm_cust_info', 'U') IS NOT NULL
+DROP TABLE bronze.crm_cust_info
+CREATE TABLE bronze.crm_cust_info(
+	cst_id int,
+	cst_key nvarchar(50),
+	cst_firstname nvarchar (50),
+	cst_lastname nvarchar (50),
+	cst_marital_status nvarchar (50),
+	cst_gndr nvarchar (50),
+	cst_create_date date
+);
+
+If object_id ('bronze.crm_prd_info', 'U') IS NOT NULL
+DROP TABLE bronze.crm_prd_info
+CREATE TABLE bronze.crm_prd_info(
+prd_id int,
+prd_key nvarchar (50),
+prd_nm nvarchar (50),
+prd_cost int,
+prd_line nvarchar (50),
+prd_start_dt date ,
+prd_end_dt date
+);
+
+
+If object_id ('bronze.crm_sales_details', 'U') IS NOT NULL
+DROP TABLE bronze.crm_sales_details
+CREATE TABLE bronze.crm_sales_details (
+    sls_ord_num  NVARCHAR(50),
+    sls_prd_key  NVARCHAR(50),
+    sls_cust_id  INT,
+    sls_order_dt INT,
+    sls_ship_dt  INT,
+    sls_due_dt   INT,
+    sls_sales    INT,
+    sls_quantity INT,
+    sls_price    INT
+);
+GO
+
+
+If object_id ('bronze.erp_loc_a101', 'U') IS NOT NULL
+DROP TABLE bronze.erp_loc_a101
+CREATE TABLE bronze.erp_loc_a101(
+cid nvarchar (50),
+cntry nvarchar(50)
+);
+
+
+If object_id ('bronze.erp_cust_az12', 'U') IS NOT NULL
+DROP TABLE bronze.erp_cust_az12
+CREATE TABLE bronze.erp_cust_az12
+(
+cid nvarchar(50),
+bdate date,
+gen nvarchar(50)
+);
+
+If object_id ('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
+DROP TABLE bronze.erp_px_cat_g1v2
+CREATE TABLE bronze.erp_px_cat_g1v2(
+id nvarchar(50),
+cat nvarchar(50),
+subcat nvarchar (50),
+maintenance nvarchar (50)
+);
